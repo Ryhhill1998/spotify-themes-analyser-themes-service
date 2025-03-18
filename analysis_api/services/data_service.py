b24db701d84cb0f78f9ea1d3a1f99413bb9a361b
ryhhill1998 <ryhhill1998@outlook.com>
2025-03-18 19:21:28 +0000

    added exception handling to data service

--- analysis_api/services/data_service.py
+++ analysis_api/services/data_service.py
@@ -1,10 +1,19 @@
 import asyncio
 import json
 
+import pydantic
+
 from analysis_api.models import EmotionalProfile, EmotionalProfileResponse, EmotionalTagsResponse, \
     EmotionalProfileRequest, EmotionalTagsRequest
-from analysis_api.services.model_service import ModelService
-from analysis_api.services.storage_service import StorageService
+from analysis_api.services.model_service import ModelService, ModelServiceException
+from analysis_api.services.storage_service import StorageService, StorageServiceException
+
+
+class DataServiceException(Exception):
+    """Base exception for errors encountered in the DataService."""
+
+    def __init__(self, message: str):
+        super().__init__(message)
 
 
 class DataService:
@@ -12,41 +21,80 @@ class DataService:
         self.model_service = model_service
         self.storage_service = storage_service
 
-    async def get_emotional_profile(self, request: EmotionalProfileRequest) -> EmotionalProfileResponse:
-        track_id = request.track_id
-        lyrics = request.lyrics
-
+    async def _get_emotional_profile_data(self, track_id: str, lyrics: str) -> dict[str, float]:
         storage_key = f"{track_id}_profile"
 
         item = await self.storage_service.retrieve_item(storage_key)
 
         if item is not None:
-            json_data = json.loads(item)
+            emotional_profile_data = json.loads(item)
         else:
             data = await asyncio.to_thread(self.model_service.generate_response, lyrics)
-            json_data = json.loads(data)
-            await self.storage_service.store_item(key=storage_key, value=json.dumps(json_data))
+            await self.storage_service.store_item(key=storage_key, value=data)
+            emotional_profile_data = json.loads(data)
 
-        emotional_profile = EmotionalProfile(**json_data)
-        analysis_response = EmotionalProfileResponse(track_id=track_id, emotional_profile=emotional_profile, lyrics=lyrics)
+        return emotional_profile_data
 
-        return analysis_response
+    async def get_emotional_profile(self, request: EmotionalProfileRequest) -> EmotionalProfileResponse:
+        track_id = request.track_id
+        lyrics = request.lyrics
 
+        try:
+            emotional_profile_data = await self._get_emotional_profile_data(track_id=track_id, lyrics=lyrics)
+
+            emotional_profile = EmotionalProfile(**emotional_profile_data)
+            analysis_response = EmotionalProfileResponse(
+                track_id=track_id,
+                emotional_profile=emotional_profile,
+                lyrics=lyrics
+            )
+
+            return analysis_response
+        except (ModelServiceException, StorageServiceException) as e:
+            message = f"Failed to retrieve emotional profile for track_id: {track_id}, lyrics: {lyrics} - {e}"
+            print(message)
+            raise DataServiceException(message)
+        except pydantic.ValidationError as e:
+            message = f"Failed to create EmotionalProfileResponse object - {e}"
+            print(message)
+            raise DataServiceException(message)
+
+    async def _get_emotional_tags_data(self, track_id: str, lyrics: str, emotion: str) -> str:
+        storage_key = f"{track_id}_tags_{emotion}"
+
+        emotional_tags_data = await self.storage_service.retrieve_item(storage_key)
+
+        if emotional_tags_data is None:
+            model_input = f"\nEmotion to Tag: {emotion}\nLyrics: {lyrics}"
+            emotional_tags_data = await asyncio.to_thread(self.model_service.generate_response, model_input)
+            emotional_tags_data = emotional_tags_data.replace("\\", "")
+            await self.storage_service.store_item(key=storage_key, value=emotional_tags_data)
+
+        return emotional_tags_data
 
     async def get_emotional_tags(self, request: EmotionalTagsRequest) -> EmotionalTagsResponse:
         track_id = request.track_id
+        lyrics = request.lyrics
         emotion = request.emotion
 
-        storage_key = f"{track_id}_tags_{emotion}"
+        try:
+            emotional_tags_data = await self._get_emotional_tags_data(track_id=track_id, lyrics=lyrics, emotion=emotion)
 
-        data = await self.storage_service.retrieve_item(storage_key)
+            emotional_tagging_response = EmotionalTagsResponse(
+                track_id=track_id,
+                emotion=emotion,
+                lyrics=emotional_tags_data
+            )
 
-        if data is None:
-            model_input = f"\nEmotion to Tag: {emotion}\nLyrics: {request.lyrics}"
-            data = await asyncio.to_thread(self.model_service.generate_response, model_input)
-            data = data.replace("\\", "")
-            await self.storage_service.store_item(key=storage_key, value=data)
-
-        emotional_tagging_response = EmotionalTagsResponse(track_id=track_id, emotion=emotion, lyrics=data)
-
-        return emotional_tagging_response
+            return emotional_tagging_response
+        except (ModelServiceException, StorageServiceException) as e:
+            message = (
+                f"Failed to retrieve emotional profile for track_id: {track_id}, lyrics: {lyrics}, "
+                f"emotion: {emotion} - {e}"
+            )
+            print(message)
+            raise DataServiceException(message)
+        except pydantic.ValidationError as e:
+            message = f"Failed to create EmotionalTagsResponse object - {e}"
+            print(message)
+            raise DataServiceException(message)
--- analysis_api/services/storage_service.py
+++ analysis_api/services/storage_service.py
@@ -1,18 +1,26 @@
 import redis.asyncio as redis
 
 
+class StorageServiceException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
 class StorageService:
     def __init__(self, client: redis.Redis):
         self.client = client
 
     async def store_item(self, key: str, value):
-        await self.client.set(name=key, value=value)
-
-    async def store_items(self, items: dict):
-        await self.client.mset(items)
+        try:
+            await self.client.set(name=key, value=value)
+        except Exception as e:
+            print(f"An exception occurred while trying to store {key}:{value} - {e}")
+            raise StorageServiceException(f"An exception occurred while trying to store {key}:{value} - {e}")
 
     async def retrieve_item(self, key: str):
-        return await self.client.get(key)
-
-    async def retrieve_items(self, keys: list[str]) -> list:
-        return await self.client.mget(keys)
+        try:
+            item = await self.client.get(key)
+            return item
+        except Exception as e:
+            print(f"An exception occurred while trying to retrieve {key} - {e}")
+            raise StorageServiceException(f"An exception occurred while trying to retrieve {key} - {e}")
